2e4cf5b447b2e758e3e1a3daa3bf3ca5315fc449
shaf_haider007 <shaf.shafheidar@gmail.com>
2025-02-02 12:18:58 +0500

    Setup the production grade connection with the DB

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require("dotenv").config({path: "/.env"});
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({path: "./.env"});
+connectDB();
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+;(async ()=> {
+    try{
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, )
+            console.log("Conectado a la base de datos");
+            app.on("error", (err) => {
+                console.error(`Error: ${err}`);
+                throw err;
+            });
+            app.listen(process.env.PORT, () => {
+                console.log(`App is listening on port ${process.env.PORT}`);
+            });
+    }
+    catch(error){
+        console.error(error);
+        throw error
+    }
+})()
+
+*/
